b69993df9e3bdd93c71ee4d86a794b4c213e11c7
DenysCherkashyn <d.a.cherkashin@yandex.ru>
2021-03-14 21:48:09 +0200

    Change input data type

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,8 +8,9 @@ public class Application {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("/************   Задание № 1  ************/");
-        new WaterMolecule(10);
-
+        new WaterMolecule("HOH");
+        System.out.println();
+        new WaterMolecule("OOHHHH");
         System.out.println("\n\n/************   Задание № 2 ************/");
 
         CustomThreadPoolExecutor customThreadPoolExecutor = new CustomThreadPoolExecutor(10);
--- src/main/java/WaterMolecule.java
+++ src/main/java/WaterMolecule.java
@@ -1,10 +1,9 @@
 package main.java;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class WaterMolecule extends Thread {
     //constants for design molecule of water
@@ -17,31 +16,28 @@ public class WaterMolecule extends Thread {
     private final CyclicBarrier moleculeBarrier = new CyclicBarrier((ALL_ATOMS_IN_MOLECULE), this::printMolecule);
     //String buffer and threads:
     private volatile StringBuffer buffer = new StringBuffer();
-    private final Callable<Boolean> oxygen = () -> releaseOxygen();
-    private final Callable<Boolean> hydrogen = () -> releaseHydrogen();
+    private final Callable<Boolean> oxygen = this::releaseOxygen;
+    private final Callable<Boolean> hydrogen = this::releaseHydrogen;
 
-    public WaterMolecule(int moleculeCount) {
+    public WaterMolecule(String atoms) {
+        List<Callable<Boolean>> tasks = generateTaskList(atoms);
+        int moleculeCount = tasks.size();
         if (moleculeCount <= 0) return;
         ExecutorService poolExecutor = Executors.
                 newFixedThreadPool(moleculeCount * (ALL_ATOMS_IN_MOLECULE));
         try {
-            poolExecutor.invokeAll(generateTaskList(moleculeCount));
+            poolExecutor.invokeAll(tasks);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         poolExecutor.shutdown();
     }
 
-    private List<Callable<Boolean>> generateTaskList(int moleculeCount) {
-        //generating list with the (moleculeCount * HYDROGEN_IN_MOLECULE) hydrogen type tasks
-        List<Callable<Boolean>> tasks = IntStream.range(0, moleculeCount * HYDROGEN_IN_MOLECULE)
-                .mapToObj(i -> hydrogen)
-                .collect(Collectors.toList());
-        //adding to previous list the (moleculeCount * OXYGEN_IN_MOLECULE) oxygen type tasks
-        IntStream.range(0, moleculeCount * OXYGEN_IN_MOLECULE).forEach(i -> tasks.add(oxygen));
-        //shuffle all tasks
-        Collections.shuffle(tasks);
-        return tasks;
+    private List<Callable<Boolean>> generateTaskList(String atoms) {
+        Function<Character, Callable<Boolean>> toCallable = (ch) -> (ch == 'H') ? hydrogen : oxygen;
+        return atoms.chars().
+                mapToObj(i -> (char) i).
+                filter(ch -> ch == 'H' || ch == 'O').map(toCallable).collect(Collectors.toList());
     }
 
     private boolean releaseOxygen() {
@@ -58,15 +54,11 @@ public class WaterMolecule extends Thread {
                 buffer.append(atom);
                 moleculeBarrier.await();
             atomBarrier.release();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (BrokenBarrierException e) {
+        } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
         return true;
     }
-
-    ;
 
     private void printMolecule() {
         System.out.print(buffer.toString() + " ");
